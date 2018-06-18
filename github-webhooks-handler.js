f39db24eb3d5644169aa21988a42dacdbd9762ff
Mihael Lenic <mihael.lenic@petra-software.com>
2018-06-18 21:04:05 +0200

    request data validation

--- github-webhooks-handler.js
+++ github-webhooks-handler.js
@@ -24,39 +24,46 @@ http
     });
 
     req.on("end", function() {
-      var parsedUrl = url.parse(req.url, true);
       
-      console.info(req.method, req.url, 'received from ', req.headers.referer , req.headers['user-agent']);
-
-      // parse request data
-      var data = JSON.parse(payload)
+      console.info(req.method, req.url, 'received from ', req.headers['user-agent']);
 
       // verify request signature
-      var isSignatureCorrect = verifySignature(req.headers['x-hub-signature'], payload)
-      if(!isSignatureCorrect) {
+      var is_signature_correct = verifySignature(req.headers['x-hub-signature'], payload)
+      if(!is_signature_correct) {
         res.writeHead(401, "Not Authorized", { "Content-Type": "text/html" });
-        res.end("401 - Not Authorized");
+        res.end("401 Not Authorized");
+        return;
+      }
+
+      // parse request data
+      var data = JSON.parse(payload);
+      var event_type = req.headers["x-github-event"];
+
+      // validate event data
+      if (!validateRequestEventData(data)) {
+        res.writeHead(400, "Bad request", { "Content-Type": "text/html" });
+        res.end("400 Bad Request");
         return;
       }
 
       // get event data from request
-      var requestEvent = {
-        repository_full_name: data && data.repository && data.repository.full_name,
-        ref: data && data.ref,
-        event: req.headers["x-github-event"]
+      var request_event = {
+        repository_full_name: data.repository.full_name,
+        ref: data.ref,
+        event: event_type
       }
 
       // handle paths
-      switch (parsedUrl.pathname) {
+      switch (req.url) {
 
         case config.server_config.path:
 
-          var matchedEvents = getMatchingEvents(requestEvent);
+          var matched_events = getMatchingEvents(request_event);
 
           // execute actions on matching events
-          if(matchedEvents.length > 0) {
+          if(matched_events.length > 0) {
 
-            matchedEvents.forEach(function(event) {
+            matched_events.forEach(function(event) {
               console.info('Executing event\'s action:', event.event_name)
               
               exec(event.action, function(error, stdout, stderr) {
@@ -73,7 +80,7 @@ http
           } else {
 
             res.writeHead(404, "Not found", { "Content-Type": "text/html" });
-            res.end("404 - Not found");
+            res.end("404 Not found");
             console.log('No matching events found.')
 
           }
@@ -81,63 +88,76 @@ http
 
         default:
           res.writeHead(404, "Not found", { "Content-Type": "text/html" });
-          res.end("404 - Not found");
+          res.end("404 Not found");
           console.log("[404] " + req.method + " to " + req.url);
       }
 
+      function validateRequestEventData(data) {
+        // console.log('validateRequestEventData', data)
+        return data && data.repository && data.repository.full_name && data.ref
+      }
+ 
       // generates a signature (HMAC hex digest of the payload. Generated using the sha1 hash function and the secret as the HMAC key) 
       // and does a time safe compare to the signature receieved in the header
-      function verifySignature(xHubSignatureHeader, payload) {
+      function verifySignature(x_hub_signature_header, payload) {
         var secret = require("./config.json").server_config.secret;
         
-        if(!xHubSignatureHeader && !secret) {
+        if(!x_hub_signature_header && !secret) {
+          console.warn('Secret is not set on this webhook. Consider setting it up to improve security.')
           return true;
-        } else if (!xHubSignatureHeader && secret) {
+        } else if (!x_hub_signature_header && secret) {
           console.error('Secret is configured on the local server but the Webhook did not contain signature header.');
           return false;
-        } else if (xHubSignatureHeader && !secret) {
+        } else if (x_hub_signature_header && !secret) {
           console.error('Webhook contains a signature header but the secret is missing from local server configuration.');
           return false;
         }
 
+        // validate x_hub_signature_header. it should be formatted like: sha1=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+        var x_hub_signature = x_hub_signature_header.split('=');
+        if(x_hub_signature.length !== 2 || x_hub_signature[0] !== 'sha1' || x_hub_signature[1].length !== 40) {
+          console.error('Request signature is not valid (wrong format).');
+          return false;
+        }
+        
         var hmac = crypto.createHmac('sha1', secret);
         hmac.update(payload);
 
         // perform a timing safe compare
-        var requestSignature = xHubSignatureHeader.split('=')[1];
-        var generatedSignature = hmac.digest('hex');
+        var request_signature = x_hub_signature[1];
+        var generated_signature = hmac.digest('hex');
 
-        // console.log('Request signature', requestSignature);
-        // console.log('Generated signature', generatedSignature);
+        // console.log('Request signature', request_signature);
+        // console.log('Generated signature', generated_signature);
 
-        var isSignatureValid = crypto.timingSafeEqual(
-          Buffer.from(generatedSignature, 'utf8'), 
-          Buffer.from(requestSignature, 'utf8'));
+        var is_signature_valid = crypto.timingSafeEqual(
+          Buffer.from(generated_signature, 'utf8'), 
+          Buffer.from(request_signature, 'utf8'));
 
-        if(!isSignatureValid) {
-          console.warn('Request signature is not valid.')
+        if(!is_signature_valid) {
+          console.error('Request signature is not valid.')
         }
 
-        return isSignatureValid;
+        return is_signature_valid;
       }
 
-      function getMatchingEvents(requestEvent) {
+      function getMatchingEvents(request_event) {
         // fetching events from config here instead of using the config loaded at startup
-        // to get the latest changes from the config file
+        // to get the current content from the config file
         var events = require("./config.json").events
 
         // TODO: implement more flexible filtering
         // - dynamic attributes matching
         // - regex values
-        var matchedEvents = events.filter(function(event){
+        var matched_events = events.filter(function(event){
 
-          return event.repository_full_name === requestEvent.repository_full_name &&
-            event.ref === requestEvent.ref &&
-            event.event === requestEvent.event;
+          return event.repository_full_name === request_event.repository_full_name &&
+            event.ref === request_event.ref &&
+            event.event === request_event.event;
 
         })
 
-        return matchedEvents;
+        return matched_events;
       }
       
     });
